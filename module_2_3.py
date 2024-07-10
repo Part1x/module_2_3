12b85d0cb2415b5a534f2c3759677758bc1fe302
Part1x <part1xtube@gmail.com>
2024-07-10 11:13:07 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,24 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+while a < len(my_list):
+    b = my_list[a]
+    if b > 0:
+        print(b)
+    elif b <= 0:
+        print('NONE')
+    a += 1
+
+
+
+
+
+#my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+#for num in my_list
+#    if num > 0:
+#        print(num)
+#    if num <= 0:
+#        print("Меньше или равно нулю")
+
+
+
+
