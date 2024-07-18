f483d28c70a208ee4972b7bbccee8667d8114a48
Part1x <part1xtube@gmail.com>
2024-07-18 18:51:12 +0300

    Add files via upload
    new

--- module_2_3.py
+++ module_2_3.py
@@ -1,12 +1,10 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 a = 0
 while a < len(my_list):
-    b = my_list[a]
-    if b > 0:
-        print(b)
-    elif b <= 0:
-        print('NONE')
-    a += 1
+    if my_list[a] > 0:
+        print(my_list[a])
+        a += 1
+    else: break
 
 
 
